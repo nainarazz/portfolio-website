856b618cd8a00dcbcd9746d88894e535f32d67c0
Naina Razafindrabiby <nainarazz@gmail.com>
2020-02-13 07:51:52 +0300

    add picture to hero component

--- src/components/sections/hero/hero.component.jsx
+++ src/components/sections/hero/hero.component.jsx
@@ -9,7 +9,7 @@ export const Hero = () => {
                     <Intro>
                         Hi, my name is Naina Razafindrabiby. I help people solve problems with code.
                     </Intro>
-                    <Photo />
+                    <Photo src="/static/images/profile-pic.jpg" />
                 </Content>
             </HeaderColor>
         </>
