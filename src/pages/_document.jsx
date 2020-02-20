9dedeb1d0109bbe79c4b365064d52d10aa9a08d0
Naina Razafindrabiby <nainarazz@gmail.com>
2020-02-20 22:59:04 +0300

    convert site to pwa

--- src/pages/_document.jsx
+++ src/pages/_document.jsx
@@ -1,6 +1,7 @@
 import Document from 'next/document';
 import React from 'react';
 import { ServerStyleSheet } from 'styled-components';
+import { Head, Main, NextScript, Html } from 'next/document';
 
 export default class MyDocument extends Document {
     static async getInitialProps(ctx) {
@@ -26,5 +27,23 @@ export default class MyDocument extends Document {
         } finally {
             sheet.seal();
         }
+    }
+
+    render() {
+        return (
+            <Html>
+                <Head>
+                    {this.props.styles}
+                    <meta charSet="utf-8" />
+                    <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+                    <link rel="manifest" href="/manifest.json" />
+                    <link rel="shortcut icon" href="/favicon.ico" />
+                </Head>
+                <body>
+                    <Main />
+                    <NextScript />
+                </body>
+            </Html>
+        );
     }
 }
