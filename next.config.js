8d8bae17a21195d9e328283a05db55697a257cec
Naina Razafindrabiby <nainarazz@gmail.com>
2020-02-24 15:35:48 +0300

    use default config for nextoffline

--- next.config.js
+++ next.config.js
@@ -1,24 +1,24 @@
 const nextOffline = require('next-offline');
 
-const nextConfig = {
-    workboxOpts: {
-        runtimeCaching: [
-            {
-                urlPattern: /^https?.*/,
-                handler: 'NetworkFirst',
-                options: {
-                    cacheName: 'offlineCache',
-                    expiration: {
-                        maxEntries: 200,
-                    },
-                },
-            },
-            {
-                urlPattern: /.png$|.jpg$|.jpeg$|.gif$/,
-                handler: 'CacheFirst',
-            },
-        ],
-    },
-};
+// const nextConfig = {
+//     workboxOpts: {
+//         runtimeCaching: [
+//             {
+//                 urlPattern: /^https?.*/,
+//                 handler: 'NetworkFirst',
+//                 options: {
+//                     cacheName: 'offlineCache',
+//                     expiration: {
+//                         maxEntries: 200,
+//                     },
+//                 },
+//             },
+//             {
+//                 urlPattern: /.png$|.jpg$|.jpeg$|.gif$/,
+//                 handler: 'CacheFirst',
+//             },
+//         ],
+//     },
+// };
 
-module.exports = nextOffline(nextConfig);
+module.exports = nextOffline();
