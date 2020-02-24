a177b2b517f5b4d5d60715df8e7519e3c897ea2d
Naina Razafindrabiby <nainarazz@gmail.com>
2020-02-24 16:27:26 +0300

    modify nextoffline config

--- next.config.js
+++ next.config.js
@@ -1,24 +1,34 @@
 const nextOffline = require('next-offline');
 
-// const nextConfig = {
-//     workboxOpts: {
-//         runtimeCaching: [
-//             {
-//                 urlPattern: /^https?.*/,
-//                 handler: 'NetworkFirst',
-//                 options: {
-//                     cacheName: 'offlineCache',
-//                     expiration: {
-//                         maxEntries: 200,
-//                     },
-//                 },
-//             },
-//             {
-//                 urlPattern: /.png$|.jpg$|.jpeg$|.gif$/,
-//                 handler: 'CacheFirst',
-//             },
-//         ],
-//     },
-// };
+const nextConfig = {
+    workboxOpts: {
+        swDest: 'static/service-worker.js',
+        runtimeCaching: [
+            {
+                urlPattern: /^https?.*/,
+                handler: 'NetworkFirst',
+                options: {
+                    cacheName: 'offlineCache',
+                    expiration: {
+                        maxEntries: 200,
+                    },
+                },
+            },
+            {
+                urlPattern: /.png$|.jpg$|.jpeg$|.gif$/,
+                handler: 'CacheFirst',
+            },
+            {
+                urlPattern: /api/,
+                handler: 'NetworkFirst',
+                options: {
+                    cacheableResponse: {
+                        statuses: [0, 200],
+                    },
+                },
+            },
+        ],
+    }
+};
 
-module.exports = nextOffline();
+module.exports = nextOffline(nextConfig);
